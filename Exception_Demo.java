322fffc49dae6746bb373dc813343222e0f46a52
Yash <yash@gmail.com>
2022-04-16 17:18:08 +0530

    Try and catch

--- Exception_Demo.java
+++ Exception_Demo.java
@@ -1,3 +1,23 @@
+import java.util.*;
 public class Exception_Demo{
     
+    
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+
+        try{
+            int c=a/b;
+            System.out.println(c);
+        }
+        catch(ArithmeticException e){
+            System.out.println(e);
+        }
+       
+        finally{
+            System.out.println("Final block");
+        }
+        System.out.println("after finally");
+    }
 }

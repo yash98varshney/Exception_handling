245ed9c2e81a7031844a39db44eb3cc2b2610ecb
Yash <yash@gmail.com>
2022-04-18 09:45:10 +0530

    added Negative Parameter Exception class and complete the exception propagation class

---
+++ ThrowandThrows/NegativeParametrException.java
@@ -0,0 +1,8 @@
+package ThrowandThrows;
+
+public class NegativeParametrException extends Exception{
+    @Override
+    public String toString(){
+        return "Dimentions cannot be negative";
+    }
+}
--- ThrowandThrows/throwandthrows.java
+++ ThrowandThrows/throwandthrows.java
@@ -2,6 +2,7 @@ package ThrowandThrows;
 
 public class throwandthrows {
     
+    // propogation of exceptions we are handling it directly in main method we can handle those in any of the methods 
     public static int meth1(){
         return 10/0;
     }
@@ -14,7 +15,36 @@ public class throwandthrows {
         meth2();
     }
 
+
+
+    // Throw and Throws example 
+
+    public static int area(int l,int b) throws NegativeParametrException{
+
+        if(l<=0 || b<=0){
+            throw new NegativeParametrException();
+        }
+        return l*b;
+    }
+
+    public static void meth4() throws NegativeParametrException{
+        System.out.println(area(-10,5));
+    }
+
     public static void main(String[] args){
-        meth3();
+        try{
+            meth3();
+        }
+        catch(Exception e){
+            System.out.println(e.toString());
+        }
+        
+        // we can use throws Exception with main also ;
+        try{
+            meth4();
+        }
+        catch(NegativeParametrException e){
+            System.out.println(e);
+        }
     }
 }
